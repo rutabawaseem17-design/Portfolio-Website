16a4b6cc87ec78164b75edf0ef7aea2d82c2988c
Rutaba Waseem <rutabawaseem17@gmail.com>
2026-08-10 10:43:19 +0500

    web

--- script.js
+++ script.js
@@ -182,3 +182,10 @@ lightbox.onclick = function(e){
 
 };
 
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
